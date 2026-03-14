aac926b194203ead0badabe9132b360a5d698cab
David Julia <dajulia3@gmail.com>
2026-03-14 02:01:26 -0600

    feat: add StatusBar component with live clock

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StatusBar from './StatusBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <StatusBar />
     </>
   )
 }
 
 export default App
+// QA run wild-fox
